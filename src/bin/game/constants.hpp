ea8ac147fbabcdd2fe9ffadacbf5f66b5f46dae8
HuyDNA <huydo862003@gmail.com>
2025-02-08 19:15:42 +0700

    feat: preload textures

--- src/bin/game/constants.hpp
+++ src/bin/game/constants.hpp
@@ -3,3 +3,6 @@
 #include <string>
 
 const inline std::string BAT_BLUE_SPRITE_PATH = std::string(ASSETS_PATH) + "/bat-blue.png";
+const inline std::string BAT_RED_SPRITE_PATH = std::string(ASSETS_PATH) + "/bat-red.png";
+const inline std::string PUCK_SPRITE_PATH = std::string(ASSETS_PATH) + "/puck.png";
+const inline std::string FIELD_SPRITE_PATH = std::string(ASSETS_PATH) + "/field.jpg";
--- src/bin/game/game.hpp
+++ src/bin/game/game.hpp
@@ -20,19 +20,39 @@ public:
       printf("Window could not be created! Error: %s\n", SDL_GetError());
       exit(1);
     }
-    SDL_SetWindowIcon(this->_window, this->_icon);
 
-    this->_renderer =
+    SDL_Renderer *_renderer =
         SDL_CreateRenderer(this->_window, -1, SDL_RENDERER_ACCELERATED);
     if (this->_renderer == NULL) {
       printf("Renderer could not be created! Error: %s\n", SDL_GetError());
+      exit(1);
     }
+
+    SDL_Surface *_icon = loadImage(BAT_BLUE_SPRITE_PATH);
+    SDL_SetWindowIcon(this->_window, this->_icon);
+
     SDL_SetRenderDrawColor(this->_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(this->_renderer);
     SDL_RenderPresent(this->_renderer);
+
+    SDL_Texture *_puck_texture = loadTexture(this->_renderer, PUCK_SPRITE_PATH);
+    SDL_Texture *_bat_blue_texture =
+        loadTexture(this->_renderer, BAT_BLUE_SPRITE_PATH);
+    SDL_Texture *_bat_red_texture =
+        loadTexture(this->_renderer, BAT_RED_SPRITE_PATH);
+    SDL_Texture *_field_texture =
+        loadTexture(this->_renderer, FIELD_SPRITE_PATH);
   }
 
-  ~Game() { SDL_DestroyWindow(this->_window); }
+  ~Game() {
+    SDL_FreeSurface(this->_icon);
+    SDL_DestroyTexture(this->_puck_texture);
+    SDL_DestroyTexture(this->_field_texture);
+    SDL_DestroyTexture(this->_bat_red_texture);
+    SDL_DestroyTexture(this->_bat_blue_texture);
+    SDL_DestroyRenderer(this->_renderer);
+    SDL_DestroyWindow(this->_window);
+  }
 
   void start() {
     bool quit = false;
@@ -49,5 +69,11 @@ public:
 private:
   SDL_Window *_window;
   SDL_Renderer *_renderer;
-  SDL_Surface *_icon = loadImage(BAT_BLUE_SPRITE_PATH);
+  SDL_Surface *_icon;
+
+private:
+  SDL_Texture *_puck_texture;
+  SDL_Texture *_bat_blue_texture;
+  SDL_Texture *_bat_red_texture;
+  SDL_Texture *_field_texture;
 };
